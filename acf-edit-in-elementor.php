80180aa593f48d26a988c5f37f2464fcccf718d3
Joel Newcomer <joelnewcomer@gmail.com>
2020-05-10 09:14:38 -0400

    Initial Commit

---
+++ acf-edit-in-elementor.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: ACF Edit in Elementor
+ * Version: 1.0.0
+ * Plugin URI: http://www.hughlashbrooke.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: acf-edit-in-elementor
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load plugin class files.
+require_once 'includes/class-acf-edit-in-elementor.php';
+require_once 'includes/class-acf-edit-in-elementor-settings.php';
+
+// Load plugin libraries.
+require_once 'includes/lib/class-acf-edit-in-elementor-admin-api.php';
+require_once 'includes/lib/class-acf-edit-in-elementor-post-type.php';
+require_once 'includes/lib/class-acf-edit-in-elementor-taxonomy.php';
+
+/**
+ * Returns the main instance of ACF_Edit_in_Elementor to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object ACF_Edit_in_Elementor
+ */
+function acf_edit_in_elementor() {
+	$instance = ACF_Edit_in_Elementor::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = ACF_Edit_in_Elementor_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+acf_edit_in_elementor();
