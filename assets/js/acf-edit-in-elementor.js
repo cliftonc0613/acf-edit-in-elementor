242e68c4315455e1b05b00918ce05c4640881754
Joel Newcomer <joelnewcomer@gmail.com>
2020-05-16 11:09:40 -0400

    dev

--- assets/js/acf-edit-in-elementor.js
+++ assets/js/acf-edit-in-elementor.js
@@ -1,12 +1,24 @@
 // Page Settings Panel - onchange save and reload elementor window.
 jQuery( function( $ ) {
 	elementor.channels.editor.on( 'RefreshPreview', function() {
-		var url = window.location.href;    
-		if (url.indexOf('?') > -1){
-	  		url += '&param=1'
-		} else {
-	 		url += '?param=1'
+		var url = window.location.href;
+		if (url.indexOf('tab') == -1) {
+			if (url.indexOf('?') > -1){
+	  			url += '&tab=settings'
+			} else {
+	 			url += '?tab=settings'
+			}
 		}
 		window.location.href = url;
 	});
+});
+
+// Re-open "Page Settings" tab if URL contains "tab=settings"
+jQuery( window ).load(function() {
+	var url = window.location.href;
+	if (url.indexOf('tab=settings') > -1){
+		jQuery( function( $ ) {
+			$e.route( 'panel/page-settings/settings' );
+		});
+	}
 });
